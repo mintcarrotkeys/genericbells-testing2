41825108a28f69afec5dcf2e2b67890faaaff5aa
mintcarrotkeys <96617003+mintcarrotkeys@users.noreply.github.com>
2022-01-09 19:53:53 +1100

    Add files via upload

--- index.js
+++ index.js
@@ -1,166 +1,63 @@
-const redirect = encodeURIComponent('https://genericbells-testing2.pages.dev');
-const appId = "genericbellstesting2";
+const projectSource = "genericbells-testing2.pages.dev";
 
-document.getElementById('version').textContent = "v0.2.9";
-
-async function requestToken() {
-    localStorage.setItem('access_age', Date.now().toString());
-    localStorage.removeItem('handle_access');
-    const codeVerifier = localStorage.getItem('handle_verifier');
-    const state = localStorage.getItem('handle_state');
-    if (codeVerifier == null) {
-        return false;
-    }
-    const params = new URLSearchParams(location.href.toString().split("?")[1]);
-    history.replaceState({}, "", "/");
-    if (params.has('code') === false) {
-        return false;
-    }
-    const code = params.get('code');
-    const returnedState = params.get('state');
-    if (returnedState !== state) {
-        return false;
-    }
-    const requestBody = (
-        "grant_type=authorization_code" +
-        "&redirect_uri=" + redirect +
-        "&client_id=" + appId +
-        "&code=" + code +
-        "&code_verifier=" + codeVerifier
+addEventListener("fetch", (event) => {
+    event.respondWith(
+        sortRequest(event.request).catch(
+            (err) => new Response(err.stack, { status: 500 })
+        )
     );
-    const requestURL = (
-        "https://student.sbhs.net.au/api/token"
-    );
+});
 
-    let response = await fetch(requestURL, {
-        method: "POST",
-        headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
-        body: requestBody});
-    if (!response.ok) {
-        console.log("1st response error");
-        response = await fetch(requestURL, {
-            method: "POST",
-            headers: {"Content-type": "application/x-www-form-urlencoded; charset=UTF-8"},
-            body: requestBody});
-        if (!response.ok) {
-            console.log('2nd response error');
-            return false;
-        }
+async function sortRequest(request) {
+    console.log("Hi");
+    if (request.method === "OPTIONS") {
+        return await respondToCors(request);
     }
-    const tokens = await response.json();
-    console.log(tokens);
-    localStorage.setItem('handle_access', tokens['access_token']);
-    localStorage.setItem('access_age', Date.now().toString());
-
-    return true;
+    else if (request.method === "GET") {
+        return await handleRequest(request);
+    }
 }
-async function requestCode() {
-    //generate code verifier 43-128 characters long
-    function randomString(length) {
-        let randomNumbers = new Uint32Array(length);
-        let verifier = "";
-        const allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-._~";
-        crypto.getRandomValues(randomNumbers);
-        for (const i of randomNumbers) {
-            verifier += allowedChars.charAt((i % 66));
-        }
-        return verifier;
-    }
-    async function digestMessage(message) {
-        const encoder = new TextEncoder();
-        const data = encoder.encode(message);
-        const hash = await crypto.subtle.digest('SHA-256', data);
-        return hash;
-    }
-    function base64url(input) {
-        let hashString = "";
-        for (const i of input) {
-            hashString += String.fromCharCode(i);
-        }
-        return btoa(hashString).replaceAll("=", "").replaceAll("+", "-").replaceAll("/", "_");
-    }
-    const codeVerifier = randomString(128);
-    const hash = await digestMessage(codeVerifier);
-    const viewHash = new Uint8Array(hash);
-    const codeChallenge = base64url(viewHash);
-    const state = randomString(32);
-    localStorage.setItem('handle_verifier', codeVerifier);
-    localStorage.setItem('handle_state', state);
 
-    const requestURL = (
-        "https://student.sbhs.net.au/api/authorize?" +
-        "client_id=" + appId + "&" +
-        "response_type=code&" +
-        "state=" + state + "&" +
-        "code_challenge=" + codeChallenge + "&" +
-        "code_challenge_method=S256&" +
-        "scope=all-ro&" +
-        "redirect_uri=" + redirect
-    );
+async function respondToCors(request) {
+    const resHeaders = {
+        "Access-Control-Allow-Origin": projectSource,
+        "Access-Control-Allow-Methods": "GET,HEAD,POST,OPTIONS",
+        "Access-Control-Max-Age": "86400",
+        "Access-Control-Allow_Headers": "content-type authorization"
+    }
 
-    location.assign(requestURL);
-
+    return new Response(null, {headers: resHeaders, status: 200, statusText: 'OK'});
 }
-document.getElementById("test").onclick = requestCode;
-async function stateManager() {
-    const params = new URLSearchParams(location.href.toString().split("?")[1]);
-    const tokenAge = localStorage.getItem('access_age');
-    const token = localStorage.getItem('handle_access');
-    console.log("token age: " + Number(tokenAge));
-    console.log("current time: " + Date.now());
-    let response;
-    if (params.has('code')) {
-        response = await requestToken();
-        if (response === false) {
-            return "askToLogin";
-        }
-        else {
-            return "success";
-        }
+
+
+async function handleRequest(request) {
+    const callables = {
+        tt: 'timetable/timetable.json',
+        idn: 'details/userinfo.json',
+        wk: 'timetable/bells.json',
+        dtt: 'timetable/daytimetable.json'
+    };
+    const point = "https://student.sbhs.net.au/api/";
+
+    const token = request.headers.get('authorization');
+    const params = new URLSearchParams(request.url.split("?")[1]);
+    if (!params.has('ask')) {
+        return new Response(null, {status: 404, statusText: "Not Found"});
     }
-    else if (tokenAge !== "" && (Date.now() <= (Number(tokenAge) + 3500000))) {
-        //check for token - if no token then show login message
-        if (token !== "") {
-            return "success";
-        }
-        else {
-            return "askToLogin";
-        }
-        //if token exist but too old then automatically redirect to login
+    const ask = params.get('ask');
+    if(!callables.hasOwnProperty(ask)) {
+        return new Response(null, {status: 404, statusText: "Not Found"});
+    }
+    if (token !== null) {
+        return fetch(point + callables[ask], {headers: {Authorization: token}});
+    }
+    else if (ask === 'wk') {
+        return fetch(point + callables[ask]);
     }
     else {
-        await requestCode();
-        return "redirect";
+        return new Response(null, {status: 404, statusText: "Not Found"});
     }
-}
-// function to request data
-async function fetchData() {
-    const baseUrl = "https://student.sbhs.net.au/api/";
-    const resources = [];
-    let routineData;
-    const token = "Bearer " + localStorage.getItem('handle_access');
-    console.log(token);
 
-    await fetch(baseUrl + "details/userinfo.json",
-        { headers: new Headers({'Authorization': token})})
-        .then(res => res.json()).then(data => routineData = data).then(() => console.log(routineData));
+
 
 }
-// if token doesn't work, delete token and start again.
-
-async function organiser() {
-    const result = await stateManager();
-    if (result === "askToLogin") {
-        document.getElementById("test").textContent = "Click to Login";
-    }
-    else if (result === "success") {
-       fetchData().then(data => console.log(data));
-    }
-    else {
-        console.log("result: " + result);
-    }
-    console.log("result: " + result);
-    console.log(localStorage.getItem('handle_access'))
-}
-
-const result = organiser();
